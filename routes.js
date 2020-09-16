e4145474ebfabae1448f20e68bab6d1223d20abd
silverioTenor <silverio_tenor@outook.com>
2020-09-16 15:32:17 -0300

    Configuração e estilização da tela de cadastro de instrutores. Início das configurações para recebimento dos dados advindos do formulário de cadastro.

--- routes.js
+++ routes.js
@@ -5,6 +5,10 @@ routes.get('/', (req, res) => res.redirect("/instructors"));
 
 routes.get('/instructors', (req, res) => res.render("instructors/index"));
 
+routes.get('/instructors/create', (req, res) => res.render("instructors/create"));
+
+routes.post('/instructors', (req, res) => res.send("Recebido!"));
+
 routes.get('/members', (req, res) => res.render("members/index"));
 
 module.exports = routes;
