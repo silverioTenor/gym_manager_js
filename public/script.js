1be203ff125fddeccb00ad6ca74aeaf4f6db8ea4
silverioTenor <silverio_tenor@outook.com>
2020-09-18 17:08:17 -0300

    Configuração do modal para confirmação do evento de exclusão do instrutor.

--- public/script.js
+++ public/script.js
@@ -1,17 +1,33 @@
 /* ======================= BUTTON ASIDE ======================= */
 
-const toggle = document.querySelector('#toggle');
+if (document.querySelector('#toggle')) {
+    const toggle = document.querySelector('#toggle');
 
-toggle.addEventListener("click", () => {
-    
-    document.querySelector('aside').classList.toggle('active_button');
+    toggle.addEventListener("click", () => {
 
-    if (toggle.querySelector('i').innerHTML == 'dehaze') {
-        toggle.querySelector('i').innerHTML = 'clear';
-        toggle.classList.add('margin');
-    }
-    else {
-        toggle.querySelector('i').innerHTML = 'dehaze';
-        toggle.classList.remove('margin');
-    }
-});
+        document.querySelector('aside').classList.toggle('active_button');
+
+        if (toggle.querySelector('i').innerHTML == 'dehaze') {
+            toggle.querySelector('i').innerHTML = 'clear';
+            toggle.classList.add('margin');
+        }
+        else {
+            toggle.querySelector('i').innerHTML = 'dehaze';
+            toggle.classList.remove('margin');
+        }
+    });
+}
+
+/* =========================== MODAL =========================== */
+
+if (document.querySelector('.options')) {
+    const modalOverride = document.querySelector('.modalOverride');
+
+    document.querySelector('.options a.delete').addEventListener("click", () => {
+        modalOverride.classList.add('active_modal');
+
+        modalOverride.querySelector('.cancel').addEventListener("click", () => {
+            modalOverride.classList.remove('active_modal');
+        });
+    });
+}
