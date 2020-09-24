c68fae71bad92da4a1037e0c8e9b2834035f0f0d
silverioTenor <silverio_tenor@outook.com>
2020-09-24 14:17:03 -0300

    Configuração e tratamento dos dados da área dos membros, bem como salvamento no DB.

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,9 +1,12 @@
-const member = require('../models/members.dao');
+const Member = require('../models/members.dao');
 const { age, date, typeBlood } = require('../../lib/utils');
 
 module.exports = {
     index(req, res) {
-        res.render("members/index", { membersList });
+        Member.getAll(members => {
+            res.render("members/index", { membersList: members });
+        });
+
     },
     create(req, res) {
         return res.render("members/create");
@@ -16,12 +19,53 @@ module.exports = {
                 return res.send("Please, fill all the fields!");
             }
         }
+
+        let { avatar_url, name, email, birth, gender, blood, weight, height } = req.body;
+
+        const data = [
+            avatar_url,
+            name,
+            email,
+            birth = date(birth).iso,
+            gender,
+            blood,
+            weight,
+            height,
+            created_at = date(Date.now()).iso
+        ]
+
+        Member.save(data, member => {
+            return res.redirect(`/members/${member.id}`);
+        });
+
     },
     show(req, res) {
-        return res.render("members/show", { member });
+        const { id } = req.params;
+
+        Member.get(id, member => {
+
+            const inst = {
+                ...member,
+                birth: `${age(member.birth)} anos`,
+                type_blood: typeBlood(member.blood),
+                created_at: date(member.created_at).br
+            }
+            return res.render("members/show", { member: inst });
+        });
     },
     update(req, res) {
-        return res.render("members/update", { member });
+        const { id } = req.params;
+
+        Member.get(id, member => {
+
+            const inst = {
+                ...member,
+                birth: date(member.birth).iso,
+                type_blood: typeBlood(member.blood)
+            }
+
+            return res.render("members/update", { member: inst });
+        });
     },
     put(req, res) {
         const keys = Object.keys(req.body);
@@ -31,6 +75,30 @@ module.exports = {
                 return res.send("Please, fill all the fields!");
             }
         }
+
+        let { id, avatar_url, name, email, birth, gender, blood, weight, height } = req.body;
+
+        const data = [
+            id,
+            avatar_url,
+            name,
+            email,
+            birth = date(birth).iso,
+            gender,
+            blood,
+            weight,
+            height
+        ];
+
+        Member.edit(data, member => {
+            return res.redirect(`/members/${member.id}`);
+        });
     },
-    delete(req, res) { }
+    delete(req, res) {
+        const { id } = req.body;
+
+        Member.remove(id);
+
+        return res.redirect("/members");
+    }
 }
--- src/app/models/members.dao.js
+++ src/app/models/members.dao.js
@@ -1,9 +1,57 @@
 const db = require('../../database/config');
 
 module.exports = {
-    get() {},
-    getAll() {},
-    save() {},
-    edit() {},
-    remove() {}
+    get(id, callback) {
+        const sql = `SELECT * FROM members WHERE id = $1;`;
+
+        db.query(sql, [id], (err, results) => {
+            if (err) { throw `Unexpected error: ${err}` };
+
+            callback(results.rows[0]);
+        });
+    },
+    getAll(callback) {
+        const sql = `
+            SELECT id, avatar_url, name, email FROM members 
+            ORDER BY id;
+        `;
+
+        db.query(sql, null, (err, results) => {
+            if (err) { throw `Unexpected error: ${err}` };
+
+            callback(results.rows);
+        });
+    },
+    save(values, callback) {
+        const sql = `
+            INSERT INTO members (avatar_url, name, email, birth, gender, blood, weight, height, created_at) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id;
+        `;
+
+        db.query(sql, values, (err, results) => {
+            if (err) { throw `Unexpected error: ${err}` };
+
+            return results.rows[0];
+        });
+    },
+    edit(values, callback) {
+        const sql = `
+            UPDATE members 
+            SET avatar_url = $2, name = $3, email = $4, birth = $5, gender = $6, blood = $7, weight = $8, height = $9
+            WHERE id = $1 RETURNING id;
+        `;
+
+        db.query(sql, values, (err, results) => {
+            if (err) { throw `Unexpected error: ${err}` };
+
+            return callback(results.rows[0]);
+        });
+    },
+    remove(id) {
+        const sql = `DELETE FROM members WHERE id = $1`;
+
+        db.query(sql, [id], err => {
+            if (err) {throw `Unexpected error: ${err}`}
+        });
+    }
 }
